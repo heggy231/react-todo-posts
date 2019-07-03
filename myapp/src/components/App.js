4b253591d49323c6647296a56f7cfa60391d52fb
Heggy Castaneda <heggy_castaneda@gap.com>
2019-07-03 11:07:46 -0700

    feat: add note how {resource} is rendered

--- myapp/src/components/App.js
+++ myapp/src/components/App.js
@@ -1,26 +1,11 @@
-// 1) import hook {useState} function
 import React, { useState } from 'react';
 
-class App {
-  state = {
-    resource: 'posts',
-    count: 0
-  }
-}
-
-// 2) add arrow function
 const App = () => {
-  // 3) delete state initialization 
-  // state = { resource: 'posts' };
-  // 4) delete render() method {}
-  // 5) add useState
   const [resource, setResource] = useState('posts');
-  const [currentCount, setCount] = useState(0);
 
   return (
     <div>
       <div>
-        {/* 6) replace this.setState to setResource('posts') and do the same for todos */}
         <button onClick={() => setResource('posts')}>
           Posts
         </button>
@@ -29,8 +14,7 @@ const App = () => {
         </button>
 
       </div>
-      {/* 7) delete `this.state.resource` add `resource` */}
-      {/* {this.state.resource} */}
+      {/* setResource will update resource value with its individual values */}
       {resource}
     </div>
   );
