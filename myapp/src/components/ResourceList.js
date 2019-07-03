ab61d32a3b1b9e25867ba41224c1accc323d0151
Heggy Castaneda <heggy_castaneda@gap.com>
2019-07-03 13:04:29 -0700

    feat: add componentDidUpdate() to only make new axios get network request only when new props were passed down.

--- myapp/src/components/ResourceList.js
+++ myapp/src/components/ResourceList.js
@@ -5,11 +5,27 @@ class ResourceList extends React.Component {
   // this.state.resources is the all the record we fetch from the api
   state = { resources: [] };
   // we want to render todo posts as soon as we render app to our screen
+  // componentDidMount() only gets invoke once only
   async componentDidMount() {
     const response = await axios.get(`https://jsonplaceholder.typicode.com/${this.props.resource}`)
 
     // axios sets all response inside of `response.data` object
     this.setState({ resources: response.data });
+  }
+
+  // lifecycle method gets called any time component renders 
+  //  b/c of parent component or we `.setState` inside of class
+  //  based component
+  async componentDidUpdate(prevProps) {
+    // if previous props were not same as current props only then run
+    if (prevProps.resource !== this.props.resource) {
+      // prevProps obj that shows the what previous props were passed in
+      // console.log(prevProps);
+      const response = await axios.get(
+        `https://jsonplaceholder.typicode.com/${this.props.resource}`
+      );
+      this.setState({ resources: response.data });
+    }
   }
 
   render() {
